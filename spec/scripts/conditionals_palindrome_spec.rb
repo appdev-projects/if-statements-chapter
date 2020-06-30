dc980f90c5d58bab58686584432f29b36856ff8a
Jelani Woods <jelani@firstdraft.com>
2020-06-30 15:00:21 -0500

    Starting point

---
+++ spec/scripts/conditionals_palindrome_spec.rb
@@ -0,0 +1,73 @@
+describe "conditionals_palindrome.rb" do
+  it "should output 'true' given the input 'HanNah'", points: 1 do
+    # Un-require conditionals_palindrome.rb
+    conditionals_palindrome = $".select{|r| r.include? 'conditionals_palindrome.rb'}
+    $".delete(conditionals_palindrome.first)
+
+    allow_any_instance_of(Object).to receive(:gets).and_return("HanNah\n")
+    response = /true/i
+
+    output = with_captured_stdout { require_relative('../../conditionals_palindrome')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(response)).to be(true),
+      "Expected output to be 'Enter one word\\ntrue', but was #{output}."
+  end
+end
+
+describe "conditionals_palindrome.rb" do
+  it "should output 'false' given the input 'candy'", points: 1 do
+    # Un-require conditionals_palindrome.rb
+    conditionals_palindrome = $".select{|r| r.include? 'conditionals_palindrome.rb'}
+    $".delete(conditionals_palindrome.first)
+
+    allow_any_instance_of(Object).to receive(:gets).and_return("candy\n")
+    response = /false/i
+    # expect { require_relative '../../conditionals_palindrome' }.to output(response).to_stdout
+
+    output = with_captured_stdout { require_relative('../../conditionals_palindrome')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(response)).to be(true),
+      "Expected output to be 'Enter one word\\nfalse', but was #{output}."
+  end
+end
+
+describe "conditionals_palindrome.rb" do
+  it "should output 'false' given the input 'racecars'", points: 1 do
+    # Un-require conditionals_palindrome.rb
+    conditionals_palindrome = $".select{|r| r.include? 'conditionals_palindrome.rb'}
+    $".delete(conditionals_palindrome.first)
+
+    allow_any_instance_of(Object).to receive(:gets).and_return("racecars\n")
+    response = /false/i
+
+    output = with_captured_stdout { require_relative('../../conditionals_palindrome')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(response)).to be(true),
+      "Expected output to be 'Enter one word\\nfalse', but was #{output}."
+  end
+end
+
+describe "conditionals_palindrome.rb" do
+  it "should output 'true' given the input 'racecar'", points: 1 do
+    # Un-require conditionals_palindrome.rb
+    conditionals_palindrome = $".select{|r| r.include? 'conditionals_palindrome.rb'}
+    $".delete(conditionals_palindrome.first)
+
+    allow_any_instance_of(Object).to receive(:gets).and_return("racecar\n")
+    response = /true/i
+
+    output = with_captured_stdout { require_relative('../../conditionals_palindrome')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(response)).to be(true),
+      "Expected output to be 'Enter one word\\ntrue', but was #{output}."
+  end
+end
+
+def with_captured_stdout
+  original_stdout = $stdout  # capture previous value of $stdout
+  $stdout = StringIO.new     # assign a string buffer to $stdout
+  yield                      # perform the body of the user code
+  $stdout.string             # return the contents of the string buffer
+ensure
+  $stdout = original_stdout  # restore $stdout to its previous value
+end
---
+++ spec/scripts/conditionals_rock_spec.rb
@@ -0,0 +1,25 @@
+describe "conditionals_rps.rb" do
+  it "should output 'You won!' given the input 'rock'", points: 1 do
+    # Un-require conditionals_rps.rb
+    conditionals_rps = $".select{|r| r.include? 'conditionals_rps.rb'}
+    $".delete(conditionals_rps.first)
+
+    allow_any_instance_of(Object).to receive(:gets).and_return("rock")
+    response = /.?Please choose rock, paper, or scissors:.?\n.?You played rock!.?\n.?The computer played scissors!.?\n.?You won!.?\n/i
+
+
+    output = with_captured_stdout { require_relative('../../conditionals_rps')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(response)).to be(true),
+      "Expected output to be 'Please choose rock, paper, or scissors:\\nYou played rock!\\n.?The computer played scissors!\\nYou won!', but was #{output}."
+  end
+end
+
+def with_captured_stdout
+  original_stdout = $stdout  # capture previous value of $stdout
+  $stdout = StringIO.new     # assign a string buffer to $stdout
+  yield                      # perform the body of the user code
+  $stdout.string             # return the contents of the string buffer
+ensure
+  $stdout = original_stdout  # restore $stdout to its previous value
+end
