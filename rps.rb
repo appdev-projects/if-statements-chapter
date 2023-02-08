8b8cde0f3c5106552addb9c549ae5a38825aa555
Ben Purinton <ben.purinton@gmail.com>
2023-02-08 22:00:42 +0000

    exercises updated and solutions added

--- rps.rb
+++ rps.rb
@@ -11,11 +11,12 @@
 #   "The computer played scissors!"
 #
 # Based on what the player chose, prints: 
+#
 #   "You won!" or "You lost!" or "You tied!"
 # 
 # If you need a refresher on the rules 
 # of Rock, Paper, Scissors: 
-# https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
+#     https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
 
---
+++ solutions.md
@@ -0,0 +1,28 @@
+# Solutions
+
+# rps.rb
+
+```ruby
+player_move = gets.chomp
+
+p "You played #{player_move}!"
+
+comp_move = "scissors"
+
+p "The computer played #{comp_move}!"
+
+if player_move == "rock"
+  p "You won!"
+elsif player_move == "paper"
+  p "You lost!"
+else
+  p "You tied!"
+end
+```
+
+# palindrome.rb
+
+```ruby
+word = gets.chomp.downcase
+p word == word.reverse
+```
